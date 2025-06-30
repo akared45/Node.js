6351bfda2af28dd8acfcfcd31221437dd35e2693
LE THANH VINH <131985083+akared45@users.noreply.github.com>
2025-06-30 08:38:41 +0700

    Update carController.js

--- car/src/infrastructure/controllers/carController.js
+++ car/src/infrastructure/controllers/carController.js
@@ -7,38 +7,53 @@ class CarController {
     this.carService = new CarService(this.carRepository);
   }
 
-  initialize() {
-    return this.carRepository.initialize();
+  async initialize() {
+    return await this.carRepository.initialize();
   }
 
-  createCar(req, res) {
-    this.carService.createCar(req.body)
-      .then(car => res.status(201).json(car))
-      .catch(error => res.status(400).json({ error: error.message }));
+  async createCar(req, res) {
+    try {
+      const car = await this.carService.createCar(req.body);
+      res.status(201).json(car);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
 
-  getCar(req, res) {
-    this.carService.getCar(req.params.id)
-      .then(car => res.status(200).json(car))
-      .catch(error => res.status(404).json({ error: error.message }));
+  async getCar(req, res) {
+    try {
+      const car = await this.carService.getCar(req.params.id);
+      res.status(200).json(car);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
   }
 
-  getAllCars(req, res) {
-    this.carService.getAllCars()
-      .then(cars => res.status(200).json(cars))
-      .catch(error => res.status(400).json({ error: error.message }));
+  async getAllCars(req, res) {
+    try {
+      const cars = await this.carService.getAllCars();
+      res.status(200).json(cars);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
 
-  updateCar(req, res) {
-    this.carService.updateCar(req.params.id, req.body)
-      .then(car => res.status(200).json(car))
-      .catch(error => res.status(404).json({ error: error.message }));
+  async updateCar(req, res) {
+    try {
+      const car = await this.carService.updateCar(req.params.id, req.body);
+      res.status(200).json(car);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
   }
 
-  deleteCar(req, res) {
-    this.carService.deleteCar(req.params.id)
-      .then(() => res.status(204).send())
-      .catch(error => res.status(404).json({ error: error.message }));
+  async deleteCar(req, res) {
+    try {
+      await this.carService.deleteCar(req.params.id);
+      res.status(204).send(); // 204: No Content
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
   }
 }
 
